edd9f2fefe214e2a99e2c833c0c5ecf628836bad
Josh Mcguigan <joshmcg88@gmail.com>
2018-07-19 02:19:32 -0700

    Added blacklist to skip post titles and urls which contain certain words

--- src/main.rs
+++ src/main.rs
@@ -29,11 +29,22 @@ fn review_subreddit(token: &str, reddit_user_agent: &str, subreddit: &str) {
 
     match rraw::new(token, reddit_user_agent, subreddit, max_reddit_submissions_to_review) {
         Ok(links) => {
-            for link in links.iter() {
+            'links: for link in links.iter() {
 
                 let time_cutoff = time::now_utc().to_timespec().sec - 60 * 60 * hours_to_go_back;
                 if (link.created_utc as i64) < time_cutoff {
                     break;
+                }
+
+                let blacklist = vec!["tutorial", "proposal", "readme", ".md"];
+                for blacklisted_word in blacklist {
+                    if link.title.to_lowercase().contains(blacklisted_word) ||
+                        link.url.to_lowercase().contains(blacklisted_word) {
+
+                        println!("Skipping post (contains blacklisted phrase): {}", link.title);
+                        continue 'links;
+
+                    }
                 }
 
                 handle_reddit_submission(token, reddit_user_agent, link);
